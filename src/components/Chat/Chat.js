6ed8e7b4e33f734ee87c1ad7f5982628ad687fce
Saptarshi <roni.bagchi@gmail.com>
2021-02-13 02:20:48 +0530

    socket modifications not working

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
+
+import TextContainer from "../TextContainer/TextContainer";
+import Messages from "../Messages/Messages";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+
 import "./Chat.css";
 
 const ENDPOINT = "localhost:5000";
@@ -13,11 +19,16 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-
+  var connectionOptions = {
+    "force new connection": true,
+    reconnectionAttempts: "Infinity",
+    timeout: 10000,
+    transports: ["websocket"]
+  };
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
-    socket = io(ENDPOINT);
+    socket = io(ENDPOINT, connectionOptions);
 
     setRoom(room);
     setName(name);
@@ -31,11 +42,19 @@ const Chat = ({ location }) => {
 
   useEffect(() => {
     socket.on("message", message => {
+      console.log(message);
       setMessages(messages => [...messages, message]);
     });
 
     socket.on("roomData", ({ users }) => {
       setUsers(users);
+    });
+  }, []);
+
+  useEffect(() => {
+    socket.on("room-message", message => {
+      console.log(message);
+      setMessages(messages => [...messages, message]);
     });
   }, []);
 
@@ -50,12 +69,15 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
         <Input
           message={message}
           setMessage={setMessage}
           sendMessage={sendMessage}
         />
       </div>
+      <TextContainer users={users} />
     </div>
   );
 };
