7d6d2119bb88ca80bb4759e90fefa9e694c0d736
Saptarshi <roni.bagchi@gmail.com>
2021-02-13 23:39:12 +0530

    modified styling

--- src/components/Messages/Message/Message.js
+++ src/components/Messages/Message/Message.js
@@ -1,6 +1,8 @@
 import "./Message.css";
 
 import ReactEmoji from "react-emoji";
+import pokemontrainer from "../../../icons/pokemontrainer.png";
+import avatar from "../../../icons/avatar.png";
 
 const Message = ({ message: { text, user }, name }) => {
   let isSentByCurrentUser = false;
@@ -12,14 +14,27 @@ const Message = ({ message: { text, user }, name }) => {
   }
 
   return isSentByCurrentUser ? (
-    <div className="messageContainer justifyEnd">
+    <div className="messageContainer mt-10 justifyEnd">
       <p className="sentText pr-10">{trimmedName}</p>
       <div className="messageBox backgroundBlue">
         <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
       </div>
+
+      <img
+        src={avatar}
+        height="30px"
+        width="30px"
+        className="incominguser-icon"
+      />
     </div>
   ) : (
-    <div className="messageContainer justifyStart">
+    <div className="messageContainer mt-10 justifyStart">
+      <img
+        src={pokemontrainer}
+        height="30px"
+        width="30px"
+        className="user-icon"
+      />
       <div className="messageBox backgroundLight">
         <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
       </div>
